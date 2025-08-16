b46eee3304eed6d3cf335a303760777be50ed2f7
Anton Jansson <anton_jansson@live.se>
2025-08-16 21:42:09 +0200

    Add image sync test

--- src/image_manager/registry_tests.rs
+++ src/image_manager/registry_tests.rs
@@ -1,9 +1,13 @@
 use std::path::Path;
+use std::str::FromStr;
+
+use croner::Cron;
 
 use crate::helpers::DataSize;
 use crate::image::Image;
 use crate::image_manager::{BuildRequest, ImageManager, ImageManagerConfig, Reference};
 use crate::reference::ImageTag;
+use crate::registry::config::RegistryUpstreamConfig;
 
 #[tokio::test]
 async fn test_pull() {
@@ -15,7 +19,7 @@ async fn test_pull() {
     let tmp_folder = helpers::get_temp_folder();
     let tmp_registry_folder = helpers::get_temp_folder();
 
-    let address: SocketAddr = "0.0.0.0:9567".parse().unwrap();
+    let address: SocketAddr = "0.0.0.0:9560".parse().unwrap();
 
     let image_tag = ImageTag::with_registry(&address.to_string(), "test", "latest");
 
@@ -83,7 +87,7 @@ async fn test_push_pull() {
     let tmp_folder = helpers::get_temp_folder();
     let tmp_registry_folder = helpers::get_temp_folder();
 
-    let address: SocketAddr = "0.0.0.0:9568".parse().unwrap();
+    let address: SocketAddr = "0.0.0.0:9561".parse().unwrap();
     tokio::spawn(crate::registry::run(create_registry_config(address, &tmp_registry_folder)));
 
     // Wait until registry starts
@@ -160,7 +164,7 @@ async fn test_push_pull_with_ref() {
     let tmp_folder = helpers::get_temp_folder();
     let tmp_registry_folder = helpers::get_temp_folder();
 
-    let address: SocketAddr = "0.0.0.0:9570".parse().unwrap();
+    let address: SocketAddr = "0.0.0.0:9562".parse().unwrap();
     tokio::spawn(crate::registry::run(create_registry_config(address, &tmp_registry_folder)));
 
     // Wait until registry starts
@@ -230,6 +234,107 @@ async fn test_push_pull_with_ref() {
     #[allow(unused_must_use)] {
         std::fs::remove_dir_all(&tmp_folder);
         std::fs::remove_dir_all(&tmp_registry_folder);
+    }
+}
+
+#[tokio::test]
+async fn test_sync() {
+    use std::net::SocketAddr;
+
+    use crate::helpers;
+    use crate::image_manager::ConsolePrinter;
+
+    let tmp_folder = helpers::get_temp_folder();
+    let tmp_primary_registry_folder = helpers::get_temp_folder();
+    let tmp_secondary_registry_folder = helpers::get_temp_folder();
+
+    let primary_address: SocketAddr = "0.0.0.0:9563".parse().unwrap();
+    let secondary_address: SocketAddr = "0.0.0.0:9564".parse().unwrap();
+
+    // Build image inside primary registry
+    let mut image = {
+        let config = ImageManagerConfig::with_base_folder(tmp_primary_registry_folder.clone());
+        let mut image_manager = ImageManager::with_config(config, ConsolePrinter::new()).unwrap();
+
+        let image = build_test_image(
+            &mut image_manager,
+            Path::new("testdata/definitions/simple4.labarfile"),
+            ImageTag::with_registry(&primary_address.to_string(), "test", "latest")
+        ).unwrap();
+        assert_eq!(Some(DataSize(3003)), image_manager.image_size(&image.hash.clone().to_ref()).ok());
+
+        image
+    };
+
+    tokio::spawn(crate::registry::run(create_registry_config(primary_address, &tmp_primary_registry_folder)));
+
+    // Wait until registry starts
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+    let mut secondary_registry_config = create_registry_config(secondary_address, &tmp_secondary_registry_folder);
+    secondary_registry_config.upstream = Some(
+        RegistryUpstreamConfig {
+            address: primary_address,
+            username: "guest".to_string(),
+            password: "guest".to_string(),
+            sync_at_startup: true,
+            sync_interval: Cron::from_str("* * * * *").unwrap(),
+        }
+    );
+    tokio::spawn(crate::registry::run(secondary_registry_config));
+
+    // Wait until registry starts
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+    let image_tag = ImageTag::with_registry(&secondary_address.to_string(), "test", "latest");
+    image.tag = image.tag.set_registry(&secondary_address.to_string());
+
+    {
+        let config = ImageManagerConfig::with_base_folder(tmp_folder.clone());
+        let mut image_manager = ImageManager::with_config(config, ConsolePrinter::new()).unwrap();
+
+        // Login
+        let login_result = image_manager.login(&secondary_address.to_string(), "guest", "guest").await;
+        assert!(login_result.is_ok(), "{}", login_result.unwrap_err());
+
+        // Wait until image exists
+        let t0 = std::time::Instant::now();
+        loop {
+            if !image_manager.list_images_registry(&secondary_address.to_string()).await.unwrap().is_empty() {
+                break;
+            }
+
+            if t0.elapsed().as_secs_f64() > 2.0 {
+                break;
+            }
+
+            tokio::time::sleep(std::time::Duration::from_millis(25)).await;
+        }
+
+        // Pull
+        let pull_result = image_manager.pull(&image_tag, None).await;
+        assert!(pull_result.is_ok(), "{}", pull_result.unwrap_err());
+        let pull_image = pull_result.unwrap();
+        assert_eq!(image, pull_image);
+
+        // List images
+        let images = image_manager.list_images();
+        assert!(images.is_ok());
+        let images = images.unwrap();
+        assert_eq!(1, images.len());
+        assert_eq!(&image_tag, &images[0].image.tag);
+
+        // Check content
+        let reference = image_tag.clone().to_ref();
+        assert_eq!(Some(DataSize(3003)), image_manager.image_size(&reference).ok());
+        let files = image_manager.list_content(&reference).unwrap();
+        assert_eq!(vec!["file1.txt".to_owned(), "file2.txt".to_owned()], files);
+    }
+
+    #[allow(unused_must_use)] {
+        std::fs::remove_dir_all(&tmp_folder);
+        std::fs::remove_dir_all(&tmp_primary_registry_folder);
+        std::fs::remove_dir_all(&tmp_secondary_registry_folder);
     }
 }
 
