aee797c1044bfeb5115ac9652dd7d2b5cae0a8e6
Anton Jansson <anton_jansson@live.se>
2025-10-12 10:11:44 +0200

    More dynamic number of parallel operations

--- src/image_manager/registry.rs
+++ src/image_manager/registry.rs
@@ -154,7 +154,9 @@ impl RegistryManager {
             }
         }
 
-        let num_parallel = if file_operations.len() >= 100 { 16 } else { 8 };
+        let bytes_per_operation = layer.storage_size.0 / file_operations.len();
+        let target_throughput = 1 * 1024 * 1024;
+        let num_parallel = (target_throughput / bytes_per_operation).clamp(4, 16);
         for chunk in file_operations.chunks(num_parallel) {
             let mut download_operations = Vec::new();
             for (source_path, content_hash, file_index) in chunk {
